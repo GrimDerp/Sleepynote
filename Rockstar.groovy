3334335e2a24e3957c2f3a87a8d277723fd843fd
BC <admin@superzen.org>
2018-10-02 19:52:08 -0600

    do stuff

--- Rockstar.groovy
+++ Rockstar.groovy
@@ -36,7 +36,49 @@ class GreetingFormatter {
         "Hello, ${name.capitalize()}"
     }
 }
-/**
+/** then:
+Add a Spock Framework test called GreetingFormatterSpec in the greeter package under src/test/groovy.
+**/
+greeting-library/src/test/groovy/greeter/GreetingFormatterSpec.groovy
+package greeter
 
+import spock.lang.Specification
+
+class GreetingFormatterSpec extends Specification {
+
+    def 'Creating a greeting'() {
+
+        expect: 'The greeting to be correctly capitalized'
+        GreetingFormatter.greeting('gradlephant') == 'Hello, Gradlephant'
+
+    }
+}
+/** then:
+Run ./gradlew build from the top-level project directory.
+
+$ ./gradlew build
+**/
+> Task :greeting-library:compileJava NO-SOURCE
+
+> Task :greeting-library:compileGroovy
+Picked up _JAVA_OPTIONS: -Xmx2048m -Xms512m
+
+> Task :greeting-library:processResources NO-SOURCE
+> Task :greeting-library:classes
+> Task :greeting-library:jar
+> Task :greeting-library:assemble
+> Task :greeting-library:compileTestJava NO-SOURCE
+> Task :greeting-library:compileTestGroovy
+> Task :greeting-library:processTestResources NO-SOURCE
+> Task :greeting-library:testClasses
+
+> Task :greeting-library:test
+Picked up _JAVA_OPTIONS: -Xmx2048m -Xms512m
+
+> Task :greeting-library:check
+> Task :greeting-library:build
+/** should produce:
+BUILD SUCCESSFUL in ~20s
+4 actionable tasks: 4 executed
 **/
 
