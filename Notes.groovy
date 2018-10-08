83a2161bb36a77aa7a443b7797e47e262acb5717
BC <admin@superzen.org>
2018-10-07 20:51:45 -0600

    Update Notes.groovy

--- Notes.groovy
+++ Notes.groovy
@@ -2,3 +2,6 @@
 gradle fails to start with JDK 11 #4591
 https://github.com/gradle/gradle/issues/4515
 **/
+
+// also, MS has open-sourced some of minecraft.java:
+// https://news.ycombinator.com/item?id=18156929
