abc62af767d68ee4b8c30837d5b43239914ee1a8
BC <admin@superzen.org>
2018-12-04 16:25:56 -0700

    Update Application.groovy

--- grails-app/init/sleepynote/Application.groovy
+++ grails-app/init/sleepynote/Application.groovy
@@ -6,5 +6,8 @@ import grails.boot.config.GrailsAutoConfiguration
 class Application extends GrailsAutoConfiguration {
     static void main(String[] args) {
         GrailsApp.run(Application, args)
+
     }
 }
+
+// decided against grails atm; using workBench3.
