6d0a0ececc663f470182a9e8811c9887a9e4df18
BC <admin@superzen.org>
2019-06-28 10:56:24 -0600

    Update IndexDB.js

--- grails-app/init/sleepynote/IndexDB.js
+++ grails-app/init/sleepynote/IndexDB.js
@@ -45,7 +45,10 @@ function addItem() {
 }
 
 /*
-This code does something very similar to previous examples in this tutorial except that it doesn't use the Promised library. We can see that the structure of the database interaction hasn't changed. Object stores are created on the database object in the upgrade event handler, and items are added to the object store in the same transaction sequence we've seen before. The difference is that this is done with requests and event handlers rather than promises and promise chains.
+This code does something very similar to previous examples in this tutorial except that it doesn't use the Promised library.
+We can see that the structure of the database interaction hasn't changed. Object stores are created on the database object in
+the upgrade event handler, and items are added to the object store in the same transaction sequence we've seen before. 
+The difference is that this is done with requests and event handlers rather than promises and promise chains.
 
 Here is a short reference of the differences between the IndexedDB API and the IndexedDB Promised library.
 
